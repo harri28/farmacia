d98f18800220c265bc141c8e840c17261082e788
harri28 <harristr13@hotmail.com>
2026-06-07 17:38:56 -0500

    Agrega favicon de GenPharma

--- includes/header.php
+++ includes/header.php
@@ -26,6 +26,7 @@ $_brand_logo_abs = $_brand_logo
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $page_title ?? $_brand_name ?></title>
+    <link rel="icon" type="image/png" href="<?= $base_path ?? '' ?>assets/img/logos/favicon.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
